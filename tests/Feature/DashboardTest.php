b00d956e17e3a41939e2e4c974ec6bc79136da0d
Mishaa <mishaa.pro@proton.me>
2025-11-27 22:05:49 +0100

    test: update dashboard test to assert redirect to Twitch page for authenticated users

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -7,10 +7,10 @@ test('guests are redirected to the login page', function () {
     $response->assertRedirect('/login');
 });
 
-test('authenticated users can visit the dashboard', function () {
+test('authenticated users can visit the dashboard and get redirected to /twitch page', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
     $response = $this->get('/dashboard');
-    $response->assertStatus(200);
+    $response->assertRedirect('/twitch');
 });
